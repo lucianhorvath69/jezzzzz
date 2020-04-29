659307a289f771367e67a2a481522d3cd2f8d53d
lucianhorvath69 <64554801+lucianhorvath69@users.noreply.github.com>
2020-04-29 21:18:00 +0200

    Add files via upload

---
+++ az.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+void main()
+{
+    int a, b, f = 1;                               // def. promnené a,b a f 
+
+    printf("Zadajte cislo pro faktorial: \n");   // Zadej cislo
+    scanf_s("%d", &b);                          // uloz ho do b 
+    for (a = 1; a <= b; a++)                   // a je definovano o velikosti 1; dokud je a mensi nebo rovno b tak pričitej k a + 1
+        f = f * a;                            // vzorec pro vypočet faktorialu
+
+    printf("Faktorial cisla: %d = %d\n", b, f);     // Vypis f 
+}                                          
